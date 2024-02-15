260780ec32355ca234acecd3af1a9b6dbdbbf2e2
giratinax <98636184+giratinax@users.noreply.github.com>
2024-02-15 13:11:07 +0530

    Create 2.Area of triangle.java

---
+++ CLASS TEST 1 /2.Area of triangle.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class GFG {
+	static double area(double h, double b)
+	{
+     return (h * b) / 2;
+	}
+	public static void main(String[] args)
+	{
+	double h = 10;
+		double b = 5;
+	System.out.println("Area of the triangle: "
+						+ area(h, b));
+	}
+}
